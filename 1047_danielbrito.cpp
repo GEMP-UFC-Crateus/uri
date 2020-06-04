c9380a79a71a2489af5320694627ce9b53e862b9
Daniel <daniell-henrique@hotmail.com>
2020-06-04 13:07:05 -0300

    Comentando soluções do primeiro contest

--- 1047_danielbrito.cpp
+++ 1047_danielbrito.cpp
@@ -10,15 +10,24 @@ int main() {
 
     cin >> initialHour >> initialMinute >> finalHour >> finalMinute;
 
+    // Turning the initial time into minutes:
     min = (initialHour*60)+initialMinute;
+
+    // Turning the final time into minutes:
     max = (finalHour*60)+finalMinute;
 
+    // Calculating the difference between this interval.    
+    // Notice that if 'max>min' we just return difference (in minutes).
+    // Otherwise, we'll have a negative number indicating that the 'final time' is less than the 'initial time'.
+    // As we're not working with 24h time, it means that it started one day and finished in the other.
+    // So, we have to convert the absolute value of this difference (in minutes) as well.
     diff = (max>min) ? max-min : ((min-max)-(24*60))*(-1);
 
     if(diff!=0){
 
         cout << "O JOGO DUROU " << diff/60 << " HORA(S) E " << diff%60  << " MINUTO(S)" << endl;
     }
+    // Notice that if the difference between 'final time' and 'initial time' is 0, it means that the game lasted for 24h0min.
     else{
 
         cout << "O JOGO DUROU 24 HORA(S) E 0 MINUTO(S)" << endl;
--- 1557_danielbrito.cpp
+++ 1557_danielbrito.cpp
@@ -1,15 +1,19 @@
 // https://www.urionlinejudge.com.br/judge/en/problems/view/1557
 
+// Note: This problem seems correct, but results in 'Presentation error' for some reason. Feel free to point the problem.
+
 #include <iostream>
 #include <vector>
 #include <math.h>
 
 using namespace std;
 
+// This function counts how many digits 'n' has:
 int countDigits(int n){
 
     int count = 0;
 
+    // Performing some arithmetic operations to extract each digit (from right to left):
     while(n>9){
 
         n/=10;
@@ -21,10 +25,13 @@ int countDigits(int n){
 
 int main(){
 
+	// This solution doesn't use matrix, but a vector to simulate its traversing:
     vector<int> dim;
+
     bool finish = false;
     int N, v, i, j, k, max, spaces;;
 
+    // Receiving a N value (until it's different of 0):
     while (!finish){
 
         cin >> N;
@@ -33,26 +40,29 @@ int main(){
         else finish = true;
     }
 
+    // This loop is use to generate each one of the squared matrices based on the previous input:
     for (v=0; v<dim.size(); v++){
 
-        // Find max number of the matrix:
-
+        // The first row of the 'matrix' starts by 1, followed by powers of 2.
+        // Then, the subsequent lines start by the respective number of the same column number.
         for (i = 1; i < pow(2, dim[v]); i *= 2){
 
             for (j = i; j < pow(2, dim[v]) * i; j *= 2){
 
+				// Stores the max value that this 'matrix' will present:
                 max = j;
             }
         }
 
+        // Now that we have the max number, we can count how digits it has (to calculate how many spaces must be printed to respect the tabulation):
         spaces = countDigits(max);
 
-        // Print matrix with tabulation:
-
+        // Finally, we print the 'matrix' with tabulation:
         for (i = 1; i < pow(2, dim[v]); i *= 2){
 
             for (j = i; j < pow(2, dim[v]) * i; j *= 2){
 
+            	// Notice that the amount of spaces to be printed is given by 'spaces-countDigits(j)':
                 for(k=0; k<spaces-countDigits(j); k++) printf(" ");
 
                 cout << j << " ";
