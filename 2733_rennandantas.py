007d347fea1f44faea3952aed2e5da76e5eb621b
Rennan Dantas <66375573+rennandantas@users.noreply.github.com>
2020-07-25 21:48:30 -0300

    2733 - The Reader's Locker

--- 2733_rennandantas.py
+++ 2733_rennandantas.py
@@ -1,28 +1,31 @@
 import collections
 
+#Armário da Carla
 armario=collections.deque()
 
 while(True):
     try:
+        #Número de livros
         n=int(input())
+        
+        #Limpando i, o contador e o armário
         i=0
         contador=0
         armario.clear()
         
+        #Para cada livro...
         while(i<n):
-            
             livro=int(input())
-            
+            #...se ele não estiver no armário
             if livro not in armario:
-                contador+=1
-                armario.appendleft(livro)
-                if len(armario)==5:
-                    armario.pop()
-            else:
-                armario.remove(livro)
-                armario.appendleft(livro)
-            i+=1
-        print(contador)    
-    
+                contador+=1 #Incrementa o contador
+                armario.appendleft(livro) #Coloca o livro no armário
+                if len(armario)==5: #Se extrapolou a capacidade do armário...
+                    armario.pop() #...então retira o livro mais a direita que é o que foi lido a mais tempo dentre os que estão no armário
+            else:#Se o livro estiver no armário
+                armario.remove(livro) #Retira da posição onde ele está...
+                armario.appendleft(livro) #...e o reinsere no armário. Esse movimento faz com que ele seja o livro mais recentemente lido.
+            i+=1#Incrementa o índide i do while
+        print(contador) #Quando sair do while, imprime o contador do caso de teste   
     except EOFError:
         break
